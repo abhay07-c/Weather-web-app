d2091f381ba6f75f5111d930281707b58f79f35a
Abhay Chavda <abhaychavda018@gmail.com>
2026-09-09 15:13:52 +0530

    Delete app directory

--- app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import*
-
-# Register your models here.
-admin.site.register(FavoriteCity)
-admin.site.register(WeatherAlert)
-admin.site.register(WeatherData)
-admin.site.register(UserSettings)
-admin.site.register(Forecast)
